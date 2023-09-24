72127bd41094be17663dd177e31eb2d6b21b6550
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-09-24 10:42:34 +0200

    Wyszukiwanie i naciesniecie Enter

--- moj_pierwszy_test_selenium.py
+++ moj_pierwszy_test_selenium.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.keys import Keys
 
 # Inicjalizacje przeglądarki Chrome
 driver = webdriver.Chrome()
@@ -9,15 +10,25 @@ driver.get('https://www.google.pl')
 
 # Maksymalizacja okna
 driver.maximize_window()
+driver.set_window_size(1600, 800)
 
 # Znajdowanie elementu by XPath - zaakceptuj zgody
 accept = driver.find_element('xpath', '//*[@id="L2AGLb"]/div')
 # Kliknięcie znalezionego elemntu
 accept.click()
 
+# Znajdź pole wyszukiwania
+search_box = driver.find_element('name', 'q')
+
+# Wprowadzanie hasła do wyszukiwarki
+search_box.send_keys('Pogoda Kraków')
+
+# Naciśnij Enter
+search_box.send_keys(Keys.ENTER)
+
 # Poczekaj 10 sekund, aby zobaczyć otwarte okno przeglądarki
 # Sleep tylko do celów debugowych, testowych, nauczania
-time.sleep(60)
+time.sleep(600)
 
 # Zakończ działanie przeglądarki
 driver.quit()  # zamknięcie wszystkich okien w przegladarce
